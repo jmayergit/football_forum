0e400abd87f3e755b3e77c530cdf4ca643be9eec
Julien Mayer <jalexismayer@gmail.com>
2015-10-08 16:34:17 -0500

    adds model associations and assign role filter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ActiveRecord::Base
+  # active record callback
+  after_save :assign_unsanctioned_status
+  # associations
+  has_one :status, dependent: :destroy
+  # validations
   validates :username, presence: true, username: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
+
+
+  private
+
+  def assign_unsanctioned_status
+    self.status = Status.create()
+  end
 end
