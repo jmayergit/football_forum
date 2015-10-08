012d9f39b789d05f33fc85b604e9b748c25e33ed
Julien Mayer <jalexismayer@gmail.com>
2015-10-08 12:58:14 -0500

    adds basic flash helper

---
+++ app/helpers/flash_helper.rb
@@ -0,0 +1,15 @@
+module FlashHelper
+  # displays flash messages
+  def flash_messages
+    if flash[:notice]
+      html = '<div class="ui positive message">
+              <p>' +
+                flash[:notice] +
+              '</p>
+            </div>'
+
+      html.html_safe
+    end
+
+  end
+end
