200219754c43204df33978fbe8484ebb2dc44c27
Julien Mayer <jalexismayer@gmail.com>
2015-10-18 11:37:08 -0500

    adds before filter to ForumsCntrller

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,2 +1,24 @@
 class ForumsController < ApplicationController
+  before_action :authenticate_admin!, only: [:new, :create, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
