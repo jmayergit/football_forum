3c169624daf7bee84b716ab20b71e1d39946d457
Julien Mayer <jalexismayer@gmail.com>
2015-10-22 13:23:39 -0500

    adds forum index code

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -2,9 +2,11 @@ class ForumsController < ApplicationController
   before_action :authenticate_admin!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
+    @forums = Forum.all
   end
 
   def new
+    @forum = Forum.new
   end
 
   def create
