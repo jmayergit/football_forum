7d02f04934c17b44b5fc027b2118556fa4bef0e8
Julien Mayer <jalexismayer@gmail.com>
2015-11-05 14:32:09 -0600

    adds js for confirmation validation

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -77,6 +77,25 @@ function validate_password(password) {
   }
 };
 
+function validate_confirmation(confirmation) {
+  var password = $(":input#user_password")[0].value;
+  var match = (confirmation === password);
+  var field = ":input#user_password_confirmation";
+  if (match) {
+    if (field in errors) {
+      remove_error(field);
+      conceal_message("#error_confirmation");
+      unpaint(field);
+    }
+  } else {
+    if (!(field in errors)) {
+      add_error(field, "Passwords do not match");
+      reveal_message("#error_confirmation");
+      paint(field);
+    }
+  }
+};
+
 $(document).on('ready page:load', function(event) {
   // console.log("non-idempotent function");
 });
@@ -97,5 +116,10 @@ $(document).on('page:change', function(event) {
   $(":input#user_password").on("blur", function(e) {
     var field = ":input#user_password";
     clear_if_empty(field);
+  });
+
+  $(":input#user_password_confirmation").on("keyup", function(e) {
+    var value = $("input#user_password_confirmation")[0].value;
+    validate_confirmation(value);
   });
 });
