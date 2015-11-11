bccedcd2402aa2f28b9c1929f2c14d84b82e8cbe
Julien Mayer <jalexismayer@gmail.com>
2015-11-11 14:16:29 -0600

    runs bin/rake db:migrate on topic

--- db/migrate/20151111201408_create_posts.rb
+++ db/migrate/20151111201408_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.text :body,                     default: "(No Text)", null: false
       t.belongs_to :topic, index: true
       t.belongs_to :user, index: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111174302) do
+ActiveRecord::Schema.define(version: 20151111201408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,17 @@ ActiveRecord::Schema.define(version: 20151111174302) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "posts", force: true do |t|
+    t.text     "body",       default: "(No Text)", null: false
+    t.integer  "topic_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["topic_id"], name: "index_posts_on_topic_id", using: :btree
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "statuses", force: true do |t|
     t.string   "value",      default: "unsanctioned", null: false
