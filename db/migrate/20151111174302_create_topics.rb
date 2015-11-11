b0cd05cf0d1f7a19671b234fde495eb44b9030d6
Julien Mayer <jalexismayer@gmail.com>
2015-11-11 13:40:31 -0600

    adds default and null false to topic migration

--- db/migrate/20151111174302_create_topics.rb
+++ db/migrate/20151111174302_create_topics.rb
@@ -1,7 +1,7 @@
 class CreateTopics < ActiveRecord::Migration
   def change
     create_table :topics do |t|
-      t.string :subject
+      t.string :subject,                      default: "", null: false
       t.belongs_to :forum,      index: true
       t.belongs_to :user,       index: true
 
