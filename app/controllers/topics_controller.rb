e7b78f3eee9a6c1a5feab8c2dac8f16f2bf444ea
Julien Mayer <jalexismayer@gmail.com>
2015-11-11 15:06:54 -0600

    adds topic create

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,4 +2,25 @@ class TopicsController < ApplicationController
   def index
     @topics = Topic.all
   end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+    if @topic.save
+      flash[:notice] = "Topic successfully created"
+      redirect_to topics_path
+    else
+      redner :new
+    end
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:subject)
+  end
 end
