91c5a508e44b0ad916d6e0a5db60493f560c3874
Julien Mayer <jalexismayer@gmail.com>
2015-12-11 14:27:51 -0600

    adds membership form

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,6 +1,16 @@
 class MembershipsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
+    @memberships = Membership.all
+  end
+
+  def new
+    @membership = Membership.new
+    @users = User.all
+    @forums = Forum.all
+  end
+
+  def create
   end
 end
