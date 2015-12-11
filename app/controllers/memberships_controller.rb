34251ed39627eecce1280237dfbf4dfd48a5a04b
Julien Mayer <jalexismayer@gmail.com>
2015-12-11 14:39:39 -0600

    adds create action to membership

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -12,5 +12,19 @@ class MembershipsController < ApplicationController
   end
 
   def create
+    @membership = Membership.new(membership_params)
+
+    if @membership.save
+      flash[:notice] = "Successfully created new membership"
+      redirect_to memberships_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def membership_params
+    params.require(:membership).permit(:user_id, :forum_id)
   end
 end
