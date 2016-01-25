39889f9e7a3e94dba3d107204c47801372b95e38
Julien Mayer <jalexismayer@gmail.com>
2016-01-25 12:38:59 -0600

    my posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,13 @@ class PostsController < ApplicationController
   include Authorize
 
   before_action :authorize_user_post, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!
+
+  def index
+    @posts = Post.where(user_id: current_user.id).paginate(page: params[:page]).order(:created_at)
+    @renderer = Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true)
+    @markdown = Redcarpet::Markdown.new(@renderer, {superscript: true, underline: true, strikethrough: true, quote: true, highlight: true})
+  end
 
   def new
     @post = Post.new
