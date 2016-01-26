5a8f23a60d0b5c5d2446c876784ed21b50addeef
Julien Mayer <jalexismayer@gmail.com>
2016-01-26 15:37:45 -0600

    adds n parameter to abbreviate helper

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -3,9 +3,9 @@ module LinkHelper
     return true if (user_signed_in? || admin_signed_in?)
   end
 
-  def abbreviate(string)
-    if string.length >= 45
-      return string[0..45] + "..."
+  def abbreviate(string, n)
+    if string.length >= n
+      return string[0..n] + "..."
     end
 
     string
