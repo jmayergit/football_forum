7fdb1d2c0895584a75ed7052bd9b2d495de895f3
Julien Mayer <jalexismayer@gmail.com>
2016-01-27 11:12:18 -0600

    adds sticky

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -2,4 +2,10 @@ $(document).on("page:change", function(e) {
   $('.message .close').on('click', function(e) {
     $(this).closest('.message').transition('fade');
   });
+
+  $('.ui.sticky')
+  .sticky({
+    context: '.ui.grid.container'
+  })
+  ;
 });
